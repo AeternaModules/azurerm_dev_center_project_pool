edc071173953f09af9cbde5419450bca9303241a
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:33:12 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ EOT
     location                                = string
     name                                    = string
     managed_virtual_network_regions         = optional(list(string))
-    single_sign_on_enabled                  = optional(bool) # Default: false
+    single_sign_on_enabled                  = optional(bool)
     stop_on_disconnect_grace_period_minutes = optional(number)
     tags                                    = optional(map(string))
   }))
