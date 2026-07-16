087eaa2138162537c28aba3a54e8696038bd9280
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:47:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,45 @@
 output "dev_center_project_pools_id" {
   description = "Map of id values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.id }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "dev_center_project_pools_dev_box_definition_name" {
   description = "Map of dev_box_definition_name values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_box_definition_name }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_box_definition_name if v.dev_box_definition_name != null && length(v.dev_box_definition_name) > 0 }
 }
 output "dev_center_project_pools_dev_center_attached_network_name" {
   description = "Map of dev_center_attached_network_name values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_center_attached_network_name }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_center_attached_network_name if v.dev_center_attached_network_name != null && length(v.dev_center_attached_network_name) > 0 }
 }
 output "dev_center_project_pools_dev_center_project_id" {
   description = "Map of dev_center_project_id values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_center_project_id }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.dev_center_project_id if v.dev_center_project_id != null && length(v.dev_center_project_id) > 0 }
 }
 output "dev_center_project_pools_local_administrator_enabled" {
   description = "Map of local_administrator_enabled values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.local_administrator_enabled }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.local_administrator_enabled if v.local_administrator_enabled != null }
 }
 output "dev_center_project_pools_location" {
   description = "Map of location values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.location }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "dev_center_project_pools_managed_virtual_network_regions" {
   description = "Map of managed_virtual_network_regions values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.managed_virtual_network_regions }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.managed_virtual_network_regions if v.managed_virtual_network_regions != null && length(v.managed_virtual_network_regions) > 0 }
 }
 output "dev_center_project_pools_name" {
   description = "Map of name values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.name }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "dev_center_project_pools_single_sign_on_enabled" {
   description = "Map of single_sign_on_enabled values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.single_sign_on_enabled }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.single_sign_on_enabled if v.single_sign_on_enabled != null }
 }
 output "dev_center_project_pools_stop_on_disconnect_grace_period_minutes" {
   description = "Map of stop_on_disconnect_grace_period_minutes values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.stop_on_disconnect_grace_period_minutes }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.stop_on_disconnect_grace_period_minutes if v.stop_on_disconnect_grace_period_minutes != null }
 }
 output "dev_center_project_pools_tags" {
   description = "Map of tags values across all dev_center_project_pools, keyed the same as var.dev_center_project_pools"
-  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.tags }
+  value       = { for k, v in azurerm_dev_center_project_pool.dev_center_project_pools : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
